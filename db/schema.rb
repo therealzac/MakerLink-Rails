6140f0a54caaaee072476ad5c2a8da6ed44ce19a
“therealzac” <Wickstrom913@gmail.com>
2016-02-24 22:24:00 -0800

    models set up

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,105 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160225062021) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cohorts", force: :cascade do |t|
+    t.integer  "school_id",               null: false
+    t.string   "name",                    null: false
+    t.date     "project_completion_date", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "cohorts", ["school_id"], name: "index_cohorts_on_school_id", using: :btree
+
+  create_table "flags", force: :cascade do |t|
+    t.integer  "dev_id",                              null: false
+    t.integer  "project_id",                          null: false
+    t.boolean  "instructor_approved", default: false, null: false
+    t.boolean  "dev_selected",        default: false, null: false
+    t.boolean  "customer_paid",       default: false, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "flags", ["dev_id"], name: "index_flags_on_dev_id", using: :btree
+  add_index "flags", ["project_id"], name: "index_flags_on_project_id", using: :btree
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "author_id",                    null: false
+    t.integer  "recipient_id",                 null: false
+    t.string   "body",                         null: false
+    t.boolean  "is_sent",      default: false, null: false
+    t.boolean  "is_trash",     default: false, null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "messages", ["author_id"], name: "index_messages_on_author_id", using: :btree
+  add_index "messages", ["recipient_id"], name: "index_messages_on_recipient_id", using: :btree
+
+  create_table "projects", force: :cascade do |t|
+    t.integer  "author_id",                   null: false
+    t.integer  "dev_id"
+    t.string   "name",                        null: false
+    t.string   "pitch",                       null: false
+    t.string   "description",                 null: false
+    t.string   "url"
+    t.integer  "view_count",      default: 0, null: false
+    t.date     "expiration_date",             null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "projects", ["author_id"], name: "index_projects_on_author_id", using: :btree
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.integer  "project_id", null: false
+    t.string   "value",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["project_id"], name: "index_tags_on_project_id", using: :btree
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "author_id",                   null: false
+    t.integer  "project_id",                  null: false
+    t.string   "body",                        null: false
+    t.string   "status",     default: "todo", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "cohort_id"
+    t.integer  "school_id"
+    t.integer  "current_project_id"
+    t.string   "first_name",         null: false
+    t.string   "last_name",          null: false
+    t.string   "password_digest",    null: false
+    t.string   "session_token",      null: false
+    t.string   "email",              null: false
+    t.string   "mobile",             null: false
+    t.string   "pic_url"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "users", ["cohort_id"], name: "index_users_on_cohort_id", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["school_id"], name: "index_users_on_school_id", using: :btree
 
 end
